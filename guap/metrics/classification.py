847ed30d31d1c765c33a9653bdbe74c9569614e3
Cyuss <maamra.youcef@gmail.com>
2021-03-21 17:22:47 +0100

    feat: ✨ define guap function skelton

---
+++ guap/metrics/classification.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+def confusion_matrix():
+    pass
+
+
+def cost_matrix():
+    pass
+
+
+def guap_metric(y_true: List[float], y_pred: List[float]):
+    return "Done"
